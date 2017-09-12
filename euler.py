bd550e1173f5be5c0641fc9613c26c78a0f752ff
xianjinru <xianjinru@meizu.com>
2017-09-12 17:11:53 +0800

    Euler.py: 引用更多prime.py的函数,增加复用性

--- euler.py
+++ euler.py
@@ -3,28 +3,7 @@
 # Author: JimruEaster<295140325@qq.com>
 # Created on 2017-09-11 16:59
 
-from prime import is_prime
-
-
-# Get greatest common divisor through Euclidean algorithm
-# param int a: an integer
-# param int b: another integer
-# return int
-def gcd(a, b):
-    if a < b:
-        a, b = b, a
-
-    while b != 0:
-        a, b = b, a % b
-    return a
-
-
-# Judge the two params are co_prime or not
-# param int m: an integer
-# param int n: another integer
-# return bool
-def is_co_prime(m, n):
-    return gcd(m,n) == 1
+from prime import *
 
 
 # Get Euler Func
@@ -37,5 +16,5 @@ def get_euler(n):
         cnt = 0
         for i in range(n):
             if is_co_prime(i, n):
-                cnt +=1
+                cnt += 1
         return cnt
